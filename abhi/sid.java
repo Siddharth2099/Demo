6bed3f095af41a22e56820d65162b4f85adea9a2
Siddharth2099 <siddharthgupta485@gmail.com>
2024-06-09 22:56:57 +0530

    in sid

--- abhi/sid.java
+++ abhi/sid.java
@@ -2,6 +2,7 @@ package abhi;
 
 public class sid {
 public static void main(String[] args) {
-    System.out.println("sidd love u alot....");
+    System.out.println("siddharth");
+    System.out.println("i love india");
 }
 }
