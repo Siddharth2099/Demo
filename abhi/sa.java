0924224e0116f79c71503086ac63234f5b340603
Siddharth2099 <siddharthgupta485@gmail.com>
2024-06-09 23:18:37 +0530

    committed

---
+++ abhi/sa.java
@@ -0,0 +1,12 @@
+package abhi;
+
+public class sa {
+    
+    public static void main(String[] args) {
+        int a,b;
+    a=10;
+    b=20;
+    System.out.println("addition is = "+a+b);
+    }
+    
+}
